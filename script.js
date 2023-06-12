13247951b7f23dc16db5f15700d9052fa4743480
unknown <tobidechamp15@gmail.com>
2023-06-12 17:40:07 +0100

    ass

---
+++ script.js
@@ -0,0 +1,49 @@
+let button = document.getElementById('ageCalculator');
+button.addEventListener('click', clickBtn);
+let app = document.getElementById('calculate');
+let dob = document.createElement('input'); 
+dob.type = "date";
+dob.id = "dobInput"
+dob.name = "dynamicInput";
+
+dob.addEventListener("change", checkDateValidity);
+
+submitButton.addEventListener("click", calculateAge)
+let calculateButton = document.createElement('button');
+calculateButton.name = "calculate"
+
+
+function calculateAge() {
+  let dobInput = document.getElementById("dobInput");
+  let dob = dobInput.value;
+  let dobDate = new Date(dob);
+  let todayDate = new Date();
+  let age = todayDate.getFullYear() - dobDate.getFullYear();
+
+  let monthDiff = todayDate.getMonth() - dobDate.getMonth();
+  let result = document.getElementById("result");
+  result.innerHTML = "Your age is " + age;
+  console.log(age);
+}
+
+function checkDateValidity() {
+  let dateInput = document.getElementById("dobInput");
+  let submitButton = document.getElementById("submitButton");
+
+  if (dateInput.validity.valid) {
+    submitButton.style.display = "block";
+  }
+  else {
+    submitButton.style.display = "none";
+  }
+
+}
+
+
+function clickBtn() {
+  dob.inputMode.includes('number');
+  //dob.innerHTML = 12;
+  app.appendChild(dob);
+  console.log(dob);
+  console.log(app);
+}
